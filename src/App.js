fffe6aa2e878b8bd08b6f83e20435704957d826f
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-11-28 21:32:08 +0200

    чистить index.js, переносить Компоненти в Арр.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './components/profile/Profile';
+import Statistics from './components/statistics/Statistics';
+import Friends from './components/friends/FriendList';
+import Transaction from './components/transaction/TransactionHistory';
+
+import users from './json/user.json';
+import statistics from './json/statistical-data.json';
+import friends from './json/friends.json';
+import transactions from './json/transactions.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile
+        name={users.name}
+        tag={users.tag}
+        location={users.location}
+        avatar={users.avatar}
+        stats={users.stats}
+      />
+      {/* // Задание 2 - Секция статистики const */}
+      {/* <Statistics /> */}
+      {/* // Задание 3 - Список друзей */}
+      {/* <Friends /> */}
+      {/* // Задание 4 - История транзакций */}
+      {/* <Transaction /> */}
     </div>
   );
 }
