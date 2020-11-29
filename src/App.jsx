978c420ad1db9e86dc4a66c741ffdcbcd86182a7
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-11-30 01:48:49 +0200

    додає компонент Sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 
 import Container from './components/container/Container';
+import Section from './components/section/Section';
 import Profile from './components/profile/Profile';
 import Statistics from './components/statistics/Statistics';
 import FriendList from './components/friends/FriendList';
@@ -14,17 +15,28 @@ import transactions from './json/transactions.json';
 function App() {
   return (
     <Container>
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={statisticalData} />
-      {/* <Statistics stats={statisticalData} /> */}
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <Section>
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Section>
+
+      <Section>
+        <Statistics title="Upload stats" stats={statisticalData} />
+        {/* <Statistics stats={statisticalData} /> */}
+      </Section>
+
+      <Section>
+        <FriendList friends={friends} />
+      </Section>
+
+      <Section>
+        <TransactionHistory items={transactions} />
+      </Section>
     </Container>
   );
 }
